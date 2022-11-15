fedb765bb2e77159eceaf5fb981ea6b91921e22a
lenilson rosa <lenilsonrr86@gmail.com>
2022-11-15 19:57:53 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,22 @@
 package application;
 
+
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department dp = new Department(1,"box");
+		Department obj = new Department(1,"box");
 		
-		System.out.println(dp);
+		
+		Seller seller = new Seller( 21, "Bob", "bob@gemail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
+		System.out.println(obj);
+		
 
 	}
 
