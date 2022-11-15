4d55f16bd3d311d7e8f94d9b37df3cd1f6ded322
lenilson rosa <lenilsonrr86@gmail.com>
2022-11-15 20:28:31 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +15,8 @@ public class Program {
 		
 		
 		Seller seller = new Seller( 21, "Bob", "bob@gemail.com", new Date(), 3000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 		System.out.println(obj);
